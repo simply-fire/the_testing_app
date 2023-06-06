84a1b8a7702dfd34e50b55b2358ccc7ce6a466bb
unknown <ashmitgupta03@gmail.com>
2023-06-06 20:33:50 +0530

    socket.io-raw-configs-in-app.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import AdminDash from './components/Body-admin-landing/Body'
 import NavbarStuDash from './components/Navbar-student-landing/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CreateCustomQuiz from './components/Create-quiz/custom'
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3000");
+
 function App() {
 
+  socket.on("rm", (message) => {
+    console.log(message);
+  })
 
   return (
     <BrowserRouter>
